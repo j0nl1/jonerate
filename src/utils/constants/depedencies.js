149ac8eebec5922e1d6d0c051a30e7bb6970e2af
Javier Aceña <40375118+j0nl1@users.noreply.github.com>
2021-02-06 18:24:16 +0100

    Features: Node TS Boilerplate (#2)
    * Add prettier, git ignore and package
    
    * Install commander, ora and fs-extra-promise as dependecies
    
    * Create bin file with basic config for node
    
    * Add package for node generation
    
    * Add babel common configuration
    
    * Add config file
    
    * Add templatePath file
    
    * Add dependencies file
    
    * Add function to create a file
    
    * Add function to create a directory
    
    * Add function to copy a template
    
    * Add logic for generate a basic node project

---
+++ src/utils/constants/depedencies.js
@@ -0,0 +1,5 @@
+const DEPENDENCIES = Object.freeze({
+  typescript: ['@babel/cli', '@babel/core', '@babel/node', '@babel/preset-typescript', '@babel/preset-env', '@types/node', 'typescript']
+});
+
+module.exports = DEPENDENCIES;
