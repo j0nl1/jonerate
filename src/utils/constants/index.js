fd09573537b3628cd2b9f6ebec34ff3f2408028b
j0nl1 <jonlitech@gmail.com>
2021-02-06 18:18:19 +0100

    Add dependencies file

---
+++ src/utils/constants/index.js
@@ -0,0 +1,5 @@
+const CONFIG = require('./config');
+const TEMPLATES = require('./templatesPath');
+const DEPENDENCIES = require('./depedencies');
+
+module.exports = { CONFIG, TEMPLATES, DEPENDENCIES };
