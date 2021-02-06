02fce3072558b57da79ce22b589decb4898245c4
j0nl1 <jonlitech@gmail.com>
2021-02-06 18:18:12 +0100

    Add templatePath file

---
+++ src/utils/constants/templatesPath.js
@@ -0,0 +1,10 @@
+const TEMPLATES = Object.freeze({
+  COMMON: {
+    babel: 'templates/common/.babelrc'
+  },
+  NODE: {
+    pkg: 'templates/node/package.json'
+  }
+});
+
+module.exports = TEMPLATES;
